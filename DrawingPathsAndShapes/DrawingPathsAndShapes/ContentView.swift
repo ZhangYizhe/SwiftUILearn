cd7d42ef41c66ab23db7595c6e0ed91bf04b5cdd
Yizhe.Zhang <enochyizhe@foxmail.com>
2019-10-18 08:27:55 +0800

    AnimatingViewsAndTransitions

---
+++ DrawingPathsAndShapes/DrawingPathsAndShapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawingPathsAndShapes
+//
+//  Created by 张艺哲 on 2019/10/16.
+//  Copyright © 2019 Elecoxy.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
