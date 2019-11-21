1b050ff1b6856ca9e6ebdfb6906461a2f4b0ecc1
Yizhe.Zhang <enochyizhe@foxmail.com>
2019-11-21 18:44:50 +0800

    优化主播间禁言论放大镜

--- 4.SwiftUI-in-Real-World/PokeMaster-Starter/PokeMaster/View/PokemonList.swift
+++ 4.SwiftUI-in-Real-World/PokeMaster-Starter/PokeMaster/View/PokemonList.swift
@@ -12,31 +12,53 @@ struct PokemonList: View {
     
     @State var expandingIndex: Int?
     
+    @State var searchContent : String = ""
+    
     var body: some View {
-        ScrollView {
-            ForEach(PokemonViewModel.all) { pokemon in
-                PokemonInfoRow(model: pokemon, expanded: self.expandingIndex == pokemon.id)
-                    .onTapGesture {
-                        if self.expandingIndex == pokemon.id {
-                            self.expandingIndex = nil
-                        } else {
-                            self.expandingIndex = pokemon.id
-                        }
+        NavigationView {
+            ScrollView {
+                
+                SearchControl
+                
+                ForEach(PokemonViewModel.all) { pokemon in
+                    PokemonInfoRow(model: pokemon, expanded: self.expandingIndex == pokemon.id)
+                        .onTapGesture {
+                            if self.expandingIndex == pokemon.id {
+                                self.expandingIndex = nil
+                            } else {
+                                self.expandingIndex = pokemon.id
+                            }
+                    }
                 }
-            }
+            }.navigationBarTitle("宝可梦列表")
         }
         
-        .overlay(
-            VStack {
-                Spacer()
-                PokemonInfoPanel(model: .sample(id: 1))
-            }.edgesIgnoringSafeArea(.bottom)
-        )
+//        .overlay(
+//            VStack {
+//                Spacer()
+//                PokemonInfoPanel(model: .sample(id: 1))
+//            }.edgesIgnoringSafeArea(.bottom)
+//        )
     }
 }
 
 struct PokemonList_Previews: PreviewProvider {
     static var previews: some View {
         PokemonList()
+    }
+}
+
+extension PokemonList {
+    var SearchControl: some View {
+        ZStack {
+            TextField("搜索宝可梦", text: $searchContent)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 5)
+                .font(.system(size: 14))
+            
+            RoundedRectangle(cornerRadius: 8).stroke(Color.init(hex: 000000, alpha: 0.2), style: StrokeStyle(lineWidth: 0.5))
+        }
+        .padding(.horizontal)
+
     }
 }
